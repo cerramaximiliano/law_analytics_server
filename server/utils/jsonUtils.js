df2965215364bcc3b6f7782baded7a42d563e664
cerramaximiliano <cerramaximiliano@gmail.com>
2023-11-27 14:35:17 -0300

    fix/delete files

--- server/utils/jsonUtils.js
+++
@@ -1,9 +0,0 @@
-function generateJSONFile(data, file) {
-    try {
-        fs.writeFileSync(DOWNLOAD_DIR + file, JSON.stringify(data))
-    } catch (err) {
-        logger.error(`Error en escritura de archivo json. ${err}`)
-    }
-};
-
-module.exports = generateJSONFile;
